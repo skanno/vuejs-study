bf190713ee7fcaa205ea058f79cc61cd1fc23ba9
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-07-30 17:13:49 +0900

    list4-12を追加。/

---
+++ list4-12/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <HelloWorld v-bind:title="message" v-bind:num="num" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: 'validate',
+      num: 1230
+    }
+  }
+}
+</script>
---
+++ list4-12/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="alert alert-primary">
+    <h2>{{ title }}</h2>
+    <p>{{ message }}</p>
+  </div>
+  <hr>
+  <p>Number: {{ num }}</p>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String,
+    num: {
+      type:Number,
+      default: 100,
+      validator: (value) => {
+        return value == parseInt(value) && value >= 0 && value <= 100;
+      }
+    }
+  },
+  data() {
+    return {
+      message: 'バリデーション・チェック'
+    }
+  }
+}
+</script>
