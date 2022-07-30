f0a507770de4731e83a421cfbe8e5c3055f137b8
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-07-30 17:36:29 +0900

    list4-13をコミット。

---
+++ list4-13/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <HelloWorld title="computed" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: 'validate',
+      num: 1230
+    }
+  }
+}
+</script>
---
+++ list4-13/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="alert alert-primary">
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
+  </div>
+  <hr>
+  <P class="h5">{{val}}</P>
+  <div class="form-group text-left">
+    <label>* 2:</label>
+    <input type="number" v-model="a" class="form-control">
+  </div>
+  <div class="form-group text-left">
+    <label>^ 2:</label>
+    <input type="number" v-model="b" class="form-control">
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String
+  },
+  data() {
+    return {
+      message: '算術プロパティの利用',
+      val: 0
+    }
+  },
+  computed: {
+    a: {
+      get() {
+        return this.val * 2;
+      },
+      set(val) {
+        this.val = Math.floor(val / 2);
+      }
+    },
+    b: {
+      get() {
+        return this.val * this.val;
+      },
+      set(val) {
+        this.val = Math.floor(Math.sqrt(val));
+      }
+    }
+  },
+  mounted() {
+    this.val = 10;
+  }
+}
+</script>
