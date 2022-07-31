086e772c27e0fbd67dec0496f23b22ca0259107a
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-07-31 15:01:06 +0900

    list4-29をコミット。

---
+++ list4-29/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="alert alert-primary">
+      <h1>{{ title }}</h1>
+      <p>{{ message }}</p>
+      <button class="btn btn-primary m-3" v-on:click="doAction">
+        {{btn}}
+      </button>
+      <transition name="transit">
+        <p v-if="flg" class="alert alert-light h5">
+          Transition context!!
+        </p>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      title: 'Trans&anim',
+      message: 'This is Transition sample!',
+      flg: true,
+      btn: 'Show/Hide'
+    }
+  },
+  methods: {
+    doAction() {
+      this.flg = !this.flg;
+    }
+  }
+}
+</script>
