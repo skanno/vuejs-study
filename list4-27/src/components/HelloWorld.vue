0886253323d70dbb0b5832dd536b232616f4c3ea
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-07-31 12:21:43 +0900

    list4-27をコミット。

---
+++ list4-27/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="alert alert-primary">
+      <h1>{{ title }}</h1>
+      <pre>{{ message }}</pre>
+    </div>
+    <hr>
+    <div alert alert-light>
+      <h3>Inner Slot</h3>
+      <ol class="list-group text-left">
+        <slot />
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      title: 'Slot',
+      message: 'This is message'
+    }
+  }
+}
+</script>
