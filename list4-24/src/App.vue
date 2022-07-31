772b87238df32908a727386ad66e06156e21fcbd
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-07-31 12:11:12 +0900

    list4-24をコミット。

---
+++ list4-24/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld>
+      <p>Begin!...</p>
+      <template v-slot:first>
+        ***First message***
+      </template>
+      <p>...Middle...</p>
+      <template v-slot:second>
+        ***Second message***
+      </template>
+      <p>..END</p>
+      <template v-slot:third>
+        ***Third message***
+      </template>
+    </HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ list4-24/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="alert alert-primary">
+      <h1>{{ title }}</h1>
+      <pre>{{ message }}</pre>
+    </div>
+    <hr>
+    <div alert alert-light>
+      <h3>Inner Slot</h3>
+      <slot />
+      <ul class="list-group">
+        <li class="list-group-item">
+          <slot name="first" />
+        </li>
+        <li class="list-group-item">
+          <slot name="second" />
+        </li>
+        <li class="list-group-item">
+          <slot name="third" />
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      title: 'Slot',
+      message: 'This is message'
+    }
+  }
+}
+</script>
