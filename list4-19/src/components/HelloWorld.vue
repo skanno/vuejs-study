7ad7166d68e96c07742b6916a52294d04a2f2b5c
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-07-31 09:17:24 +0900

    list4-19をコミット。

---
+++ list4-19/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="alert alert-primary">
+    <h1>{{ title }}</h1>
+    <pre v-on:click="clear">{{ message }}</pre>
+  </div>
+  <hr>
+  <div>
+    <input type="text" v-on:keydown="type" class="form-control">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      title: 'Event',
+      message: ''
+    }
+  },
+  methods: {
+    type(event) {
+      this.message += event.key + ' ';
+      if (event.key == 'Escape') {
+        this.message = '';
+      }
+      event.target.value = '';
+    },
+    clear() {
+      this.message = '';
+    }
+  }
+}
+</script>
