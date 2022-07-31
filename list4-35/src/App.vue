ab0b9a7cba4055b7189c936279aa60e590f32e0a
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-07-31 16:10:17 +0900

    list4-35をコミット

---
+++ list4-35/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld>
+      <li class="list-group-item" v-for="obj in slotobjs" v-bind:key="obj.name">
+        {{obj.name}} ({{obj.mail}})
+      </li>
+    </HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      slotobjs: [
+        {name: 'Taro', mail: 'taro@yamada'},
+        {name: 'Hanako', mail: 'hanako@flowor'},
+        {name: 'Sachiko', mail: 'sachiko@happy'}
+      ]
+    }
+  }
+}
+</script>
