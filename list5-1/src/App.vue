8bb1412b9fe2d49b54f083a737e885c3b1da5a48
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-07-31 16:50:47 +0900

    list5-1をコミット。

---
+++ list5-1/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <HelloWorld title="Composition API" msg="This is Composition API sample." />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ list5-1/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="alert alert-primary">
+      <h1>{{ title }}</h1>
+      <p>{{ msg }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    msg: String
+  },
+  setup(props) {
+    console.log(props);
+  }
+}
+</script>
