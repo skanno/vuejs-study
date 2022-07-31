9895e9b8e745851d34d2b205b6ae62fb617aceb0
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-07-31 11:48:19 +0900

    list4-23をコミット。

---
+++ list4-23/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <HelloWorld title="slot" />
+    <p>***this is defalut text***</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ list4-23/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="alert alert-primary">
+      <h1>{{ title }}</h1>
+      <pre>{{ message }}</pre>
+    </div>
+    <hr>
+    <div alert alert-light>
+      <h3>Inner Slot</h3>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      title: 'Slot',
+      message: 'This is message'
+    }
+  }
+}
+</script>
