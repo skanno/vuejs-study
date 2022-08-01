39a5d48aac1dfaf4d4f5950abe3a30b340d43b7c
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-08-01 19:02:16 +0900

    list5-5をコミット。

---
+++ list5-5/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script>
+import {h} from 'vue';
+
+export default {
+  props: {
+    title: String,
+    msg: String
+  },
+  setup(props) {
+    return () => h(
+      'div',
+      {class: 'alert alert-primary'},
+      [
+        h('h1', props.title),
+        h('p', props.msg)
+      ]);
+  }
+}
+</script>
