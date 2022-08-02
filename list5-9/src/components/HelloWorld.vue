427013954c637aab96d0120ad50b928b61bb4fd5
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-08-02 18:02:40 +0900

    list5-9をコミット。

---
+++ list5-9/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="alert alert-primary">
+    <h1>{{title}}</h1>
+    <p class="h5">{{data.msg}}</p>
+  </div>
+</template>
+
+<script>
+import {reactive} from 'vue';
+
+export default {
+  props: {
+    title: String
+  },
+  setup(props, context) {
+    console.log(context);
+
+    const data = reactive({
+      msg: 'This is ref-value!'
+    });
+
+    data.msg = context.attrs['msg'].toUpperCase();
+
+    return {data};
+  }
+}
+</script>
