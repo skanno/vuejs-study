41a985eb9524c89b3dea73bfc72ea77a0db924f2
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-08-07 10:19:47 +0900

    list5-27をコミット。

---
+++ list5-27/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="alert alert-primary">
+    <h1>{{data.title}}</h1>
+    <p class="mt-3 h5">{{$store.state.message}}</p>
+    <hr>
+    <div class="btn btn-secondary" @click.exact="$store.commit('count')" @click.ctrl="$store.commit('reset')">
+      clicked: {{ $store.state.counter }}
+    </div>
+  </div>
+</template>
+
+<script>
+import {reactive} from 'vue';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    name: String
+  },
+  setup(props, context) {
+    const data = reactive({
+      title: 'Veux',
+    });
+
+    return {data};
+  }
+}
+</script>
