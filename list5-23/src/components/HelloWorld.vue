ee170a75d2d952482e817cbaee8ec5a0756752d5
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-08-07 09:54:48 +0900

    list5-23をコミット

---
+++ list5-23/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="alert alert-primary">
+    <h1>{{data.title}}</h1>
+    <p class="mt-3 h5">{{$store.state.message}}</p>
+  </div>
+</template>
+
+<script>
+import {reactive} from 'vue';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    name: String
+  },
+  setup(props, context) {
+    const data = reactive({
+      title: 'Veux',
+    });
+
+    return {data};
+  }
+}
+</script>
