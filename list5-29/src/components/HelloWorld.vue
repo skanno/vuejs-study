91a97e373c099999a09c79391559d6b747003638
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-08-07 14:10:33 +0900

    list5-29をコミット。

---
+++ list5-29/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="alert alert-primary">
+    <h1>{{data.title}}</h1>
+    <p class="mt-3 h5">{{$store.state.message}}</p>
+    <hr>
+    <div class="alert alert-dark"
+      @click.exact="$store.commit('count', 1)"
+      @click.shift="$store.commit('count', 2)"
+      @click.ctrl="$store.commit('count', 3)">
+      <a class="h5" @click.stop="$store.commit('reset')">
+        clicked: {{ $store.state.counter }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import {reactive} from 'vue';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    name: String
+  },
+  setup(props, context) {
+    const data = reactive({
+      title: 'Veux',
+    });
+
+    return {data};
+  }
+}
+</script>
---
+++ list5-29/src/store.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex';
+
+export const store = createStore({
+  state() {
+    return {
+      message: 'This is store data.',
+      counter: 0
+    }
+  },
+  mutations: {
+    count: (state, n) => {
+      state.counter += n;
+    },
+    reset: (state) => {
+      state.counter = 0;
+    }
+  }
+});
