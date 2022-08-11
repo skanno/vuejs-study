d58d04b16aa37070c2ebee101095207fde48fdc5
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-08-11 15:43:25 +0900

    list6-7を追加。

---
+++ list6-7/src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="alert alert-primary">
+    <h1>{{data.title}}</h1>
+    <p>{{data.message}}</p>
+    <div class="form-inline my-2">
+      <input type="text" v-model="data.find" class="form-control">
+      <button @click="getData" class="btn btn-primary">Click</button>
+    </div>
+    <div class="alert alert-light">
+      {{data.fire_data}}
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+import { reactive, onMounted } from 'vue';
+
+let url = 'https://vue3-study-5295f-default-rtdb.asia-southeast1.firebasedatabase.app/person/';
+
+export default {
+  setup(props) {
+    const data = reactive({
+      title: 'Firebase',
+      message: 'This is Firebase sample.',
+      find: '',
+      fire_data: {}
+    });
+
+    const getData = () => {
+      console.log(data.find !== '');
+
+      if (data.find !== '') {
+        console.log(url + data.find + '.json');
+
+        axios.get(url + data.find + '.json').then((result) => {
+          data.fire_data = result.data;
+        }).catch((error) => {
+          data.message = 'Error';
+          data.fire_data = {};
+        });
+      } else {
+        data.fire_data = 'no data found...';
+      }
+    };
+
+    onMounted(() => {
+      getData();
+    });
+
+    return {data, getData};
+  }
+}
+</script>
